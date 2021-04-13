73e276e71a39540aa46eb2ddf62b1d33fc8900ed
Bryan <Bryan.s.pawlowski@gmail.com>
2021-04-12 22:05:28 -0700

    Adding base C++ project to BonkersVisualizer
    Will remove cmake-build-debug folder after this...

---
+++ BonkersVisualizer/cmake-build-debug/CMakeFiles/BonkersVisualizer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "BonkersVisualizer.exe"
+  "BonkersVisualizer.exe.manifest"
+  "BonkersVisualizer.lib"
+  "BonkersVisualizer.pdb"
+  "CMakeFiles/BonkersVisualizer.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BonkersVisualizer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ BonkersVisualizer/cmake-build-debug/CMakeFiles/BonkersVisualizer.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for BonkersVisualizer.
+# This may be replaced when dependencies are built.
